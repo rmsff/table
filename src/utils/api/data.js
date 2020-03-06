e8632d0e4164aa47f9bc1c0277892033070c64dc
Serikoff <serikoff@li.ru>
2020-03-06 20:30:16 +0300

    Update data.js

--- src/utils/api/data.js
+++ src/utils/api/data.js
@@ -3,6 +3,6 @@ import { axios } from 'core';
 export default {
 	getData: (count = 100) =>
 		axios.get(
-			`http://www.filltext.com/?rows=${count}&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`
+			`https://cors-anywhere.herokuapp.com/http://www.filltext.com/?rows=${count}&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`
 		),
 };
